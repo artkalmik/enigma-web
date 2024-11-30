2441b8afbc84676185e07b92b65139c32c98a23b
Artkalm <artkalm@icloud.com>
2024-11-30 19:03:24 +0300

    Update TypeScript and package configurations. Added "type": "module" to package.json, refined build script, and included new dependencies for Vue and TypeScript. Updated tsconfig files for improved module resolution and compiler options. Cleaned up env.d.ts and shims-vue.d.ts for better type definitions.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_WS_URL: string
